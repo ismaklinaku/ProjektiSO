eee2e6cfef6b9989999bee684462c56cd4f77f1c
gyltenesfishta <121941850+gyltenesfishta@users.noreply.github.com>
2023-05-29 17:47:38 +0300

    Update server.c

--- server.c
+++ server.c
@@ -146,3 +146,84 @@ void *handle_client(void *arg){
 
 		bzero(buff_out, BUFFER_SZ);
 	}
+	/* Delete client from queue and yield thread */
+	close(cli->sockfd);
+  queue_remove(cli->uid);
+  free(cli);
+  cli_count--;
+  pthread_detach(pthread_self());
+
+	return NULL;
+}
+
+int main(int argc, char **argv){
+	if(argc != 2){
+		printf("Usage: %s <port>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	char *ip = "127.0.0.1";
+	int port = atoi(argv[1]);
+	int option = 1;
+	int listenfd = 0, connfd = 0;
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in cli_addr;
+  pthread_t tid;
+
+  /* Socket settings */
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(ip);
+  serv_addr.sin_port = htons(port);
+
+  /* Ignore pipe signals */
+	signal(SIGPIPE, SIG_IGN);
+
+	if(setsockopt(listenfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0){
+		perror("ERROR: setsockopt failed");
+    return EXIT_FAILURE;
+	}
+
+	/* Bind */
+  if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("ERROR: Socket binding failed");
+    return EXIT_FAILURE;
+  }
+
+  /* Listen */
+  if (listen(listenfd, 10) < 0) {
+    perror("ERROR: Socket listening failed");
+    return EXIT_FAILURE;
+	}
+
+	printf("=== WELCOME TO THE CHATROOM ===\n");
+
+	while(1){
+		socklen_t clilen = sizeof(cli_addr);
+		connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
+
+		/* Check if max clients is reached */
+		if((cli_count + 1) == MAX_CLIENTS){
+			printf("Max clients reached. Rejected: ");
+			print_client_addr(cli_addr);
+			printf(":%d\n", cli_addr.sin_port);
+			close(connfd);
+			continue;
+		}
+
+		/* Client settings */
+		client_t *cli = (client_t *)malloc(sizeof(client_t));
+		cli->address = cli_addr;
+		cli->sockfd = connfd;
+		cli->uid = uid++;
+
+		/* Add client to the queue and fork thread */
+		queue_add(cli);
+		pthread_create(&tid, NULL, &handle_client, (void*)cli);
+
+		/* Reduce CPU usage */
+		sleep(1);
+	}
+
+	return EXIT_SUCCESS;
+}
